35384096b0df9735ebb513ae3f90de9e7e4ee421
JC Jamison <jcjamison@JCs-MacBook-Air.local>
2022-10-13 13:52:04 -0600

    send text

---
+++ components/SendText.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, TextInput, Button, Alert } from "react-native";
+
+const UselessTextInput = () => {
+  const [text, onChangeText] = React.useState(null);
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="phone number"
+      />
+      <Button
+        title="send text"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+export default UselessTextInput;
